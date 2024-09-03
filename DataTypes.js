64b9a1bea864ae3a955dcbd184ba73fe2e63f0b1
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-09-03 21:51:20 +0530

    Data Types

---
+++ DataTypes.js
@@ -0,0 +1,63 @@
+//Primitive Data Types & Object Type
+
+//------------------------------------------------//
+// Numbers Data Type
+var number = 10
+var number2 = 40
+var number3 = 35.5
+var number4 = 90.009
+
+console.log(number4)
+
+//String Data Type
+
+var Name  = "Subhadeep"
+var series  = "JavaScript Practice Session"
+
+console.log(Name)
+console.log(series)
+
+//Boolean Data Types
+var isAvailable = true
+var isNumber = false
+console.log(isAvailable)
+
+//Undefined
+var NotDeclare = undefined
+console.log(NotDeclare)
+
+//Null Data Types
+var NoValue = null
+console.log(NoValue)
+
+//BigInt
+var LargeNumber = BigInt(9000000000000000000000);
+console.log(LargeNumber)
+
+//---------------------------------------------------//
+//Object Data Type
+
+var Person ={
+    Name:"Subhadeep",
+    Age : 21,
+    Country: "India",
+    University:"VIT"
+}
+console.log(Person)
+console.log(Person["Name"])
+console.log(Person["Age"])
+console.log(Person["Country"])
+console.log(Person["University"])
+
+//Array
+var Number = [1,2,3,4,5]
+console.log(Number)
+
+//Function (Special type of Object)
+function Greet(){
+    console.log("Happy Birthday")
+}
+Greet()
+
+
+
