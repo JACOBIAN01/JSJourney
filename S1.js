72dcb7b6632c1425a7f4775c398541d097869543
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-09-03 21:36:34 +0530

    S1

--- S1.js
+++ S1.js
@@ -1,1 +1,20 @@
-console.log("Hello JS!")
+// console.log("Hello!")
+//Number Data Type
+var number1 = 20;
+var number2 = 10;
+//Data Types String
+var str1 = "This is a String";
+var str = "New String"
+//Boolean
+var value = true;
+// Data Type Object
+var Mark={
+    Math : 90,
+    English : 84,
+    History : 98,
+    Bengali : 86,
+}
+//Array
+var My_Array = [1,2,3,4,5]
+var new_array = [8,6]
+var names = ["Chiku","Nabin","Sachil","Mitin"]
